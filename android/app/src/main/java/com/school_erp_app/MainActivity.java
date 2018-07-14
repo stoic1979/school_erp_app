b868374200002bafaac2961891d8bf7485416d76
Navjot Singh <weavebytes@gmail.com>
2018-07-15 00:05:12 +0530

    adding bare bones of app

---
+++ android/app/src/main/java/com/school_erp_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.school_erp_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "school_erp_app";
+    }
+}
